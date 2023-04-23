5eb88b833c60baf17aa270facf9bb8417ea7beda
Ole <olebitnes@gmail.com>
2023-04-23 21:33:56 +0200

    test 4

--- github3/ContentView.swift
+++ github3/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world! test3")
+            Text("Hello, world! test3 test4")
         }
         .padding()
     }
