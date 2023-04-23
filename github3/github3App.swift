85d7db2ef250e36e0ea98e70cd893e033906aff8
Ole <olebitnes@gmail.com>
2023-04-23 21:32:26 +0200

    Initial Commit

---
+++ github3/github3App.swift
@@ -0,0 +1,17 @@
+//
+//  github3App.swift
+//  github3
+//
+//  Created by Default User on 23/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct github3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
